cc05543ce2aac1997b2834b738c4f15832d02ed7
Huy Phan <enpiech@gmail.com>
2019-06-10 09:33:21 +0700

    no message

---
+++ app/src/main/java/tdc/edu/vn/wifichecklist/WifiListActivity.java
@@ -0,0 +1,14 @@
+package tdc.edu.vn.wifichecklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WifiListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wifi_list_layout);
+    }
+}
