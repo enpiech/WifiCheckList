da046e38368794349d5220e6f98d709d2e6f704f
Huy Phan <enpiech@gmail.com>
2019-06-11 17:03:45 +0700

    Get surrounding wifis data from device Connect to wifi

--- app/src/main/java/tdc/edu/vn/wifichecklist/Controller/WifiDetailActivity.java
+++ app/src/main/java/tdc/edu/vn/wifichecklist/Controller/WifiDetailActivity.java
@@ -2,23 +2,42 @@ package tdc.edu.vn.wifichecklist.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import tdc.edu.vn.wifichecklist.Model.Wifi;
 import tdc.edu.vn.wifichecklist.R;
 
 public class WifiDetailActivity extends AppCompatActivity {
-    private TextView txtSsid;
-    private TextView txtBssid;
-    private TextView txtSecurity;
-    private TextView txtFrequency;
-    private TextView txtRssi;
+    private ListView lstWifiProperties;
+    private Button btnConnect;
+    private EditText edtPassword;
+
+    private Wifi wifiData;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> dataSet;
+
+    private WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wifi_detail_layout);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        dataSet = new ArrayList<>();
 
         loadView();
 
@@ -30,23 +49,73 @@ public class WifiDetailActivity extends AppCompatActivity {
         assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        txtSsid = findViewById(R.id.txtSsid);
-        txtBssid = findViewById(R.id.txtBssid);
-        txtSecurity = findViewById(R.id.txtSecurity);
-        txtFrequency = findViewById(R.id.txtFrequency);
-        txtRssi = findViewById(R.id.txtRssi);
+        lstWifiProperties = findViewById(R.id.lstWifiProperties);
+        btnConnect = findViewById(R.id.btnConnect);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                prepareToConnectWifi(wifiData);
+            }
+        });
     }
 
     void loadData() {
-        Wifi wifiData = (Wifi) getIntent().getSerializableExtra("wifiData");
+        wifiData = (Wifi) getIntent().getSerializableExtra("wifiData");
 
         if (wifiData != null) {
-            txtSsid.setText(wifiData.getSsid());
-            txtBssid.setText(wifiData.getBssid());
-            txtSecurity.setText(wifiData.getSecurity());
-            txtFrequency.setText(wifiData.getFrequency() + "");
-            txtRssi.setText(wifiData.getRssi() + "");
+            dataSet.add("SSID: " + wifiData.getSsid());
+            dataSet.add("BSSID: " + wifiData.getBssid());
+            dataSet.add("Security: " + wifiData.getSecurity());
+            dataSet.add("Frequency: " + wifiData.getFrequency());
+            dataSet.add("RSSI: " + wifiData.getRssi());
         }
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataSet);
+        lstWifiProperties.setAdapter(arrayAdapter);
+    }
+
+    private void prepareToConnectWifi(final Wifi wifi) {
+        final String password = edtPassword.getText().toString();
+        connectToWifi(wifi, password);
+    }
+
+    private void connectToWifi(Wifi wifi, String password) {
+        WifiConfiguration wifiConfiguration = new WifiConfiguration();
+        wifiConfiguration.SSID = wifi.getSsid();
+        wifiConfiguration.preSharedKey = String.format("\"%s\"", password);
+
+        // netId == -1 if connected failed
+        int netId = wifiManager.addNetwork(wifiConfiguration);
+
+        if (netId != -1) {
+            wifiManager.disconnect();
+            wifiManager.enableNetwork(netId, true);
+            wifiManager.reconnect();
+
+            wifiConfiguration.SSID = "\"\"" + wifi.getSsid() + "\"\"";
+            wifiConfiguration.preSharedKey = "\"\"" + password + "\"\"";
+            wifiManager.addNetwork(wifiConfiguration);
+
+            saveWifi(wifi, password);
+
+            Toast.makeText(this, "Connect success to " + wifi.getWifiName(), Toast.LENGTH_SHORT).show();
+        } else {
+
+            edtPassword.setText("");
+            edtPassword.setError("Wrong password");
+            edtPassword.requestFocus();
+
+            Toast.makeText(this, "Wrong password! Connect failed to " + wifi.getWifiName(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void saveWifi(Wifi wifi, String password) {
+        //TODO Save password and wifi name to SQLite
+
+
     }
 
     @Override
--- app/src/main/java/tdc/edu/vn/wifichecklist/Controller/WifiListActivity.java
+++ app/src/main/java/tdc/edu/vn/wifichecklist/Controller/WifiListActivity.java
@@ -1,35 +1,128 @@
 package tdc.edu.vn.wifichecklist.Controller;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import tdc.edu.vn.wifichecklist.Adapter.ItemClickListener;
 import tdc.edu.vn.wifichecklist.Adapter.WifiAdapter;
-import tdc.edu.vn.wifichecklist.Helper.GetWifiDataFromJson;
 import tdc.edu.vn.wifichecklist.Model.Wifi;
 import tdc.edu.vn.wifichecklist.R;
 
 public class WifiListActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE_ACCESS_LOCATION = 0;
     private ListView lstWifi;
     private ArrayList<Wifi> wifiList;
-    private static WifiAdapter adapter;
+    private WifiAdapter adapter;
     private Button btnDetail;
+
+    private WifiManager wifiManager;
+    private LocationManager locationManager;
+
+    private BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(action)) {
+                List<ScanResult> results = wifiManager.getScanResults();
+                wifiList.clear();
+                for (ScanResult result : results) {
+                    wifiList.add(new Wifi(result.SSID, result.BSSID, result.capabilities, result.frequency, result.level));
+                }
+                Collections.sort(wifiList, new Comparator<Wifi>() {
+                    @Override
+                    public int compare(Wifi wifi, Wifi t1) {
+                        return t1.getRssi() - wifi.getRssi();
+                    }
+                });
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wifi_list_layout);
 
-        loadView();
+        loadServices();
 
+        scanWifiEvery(30);
+
+        loadView();
         loadData();
+    }
+
+    private void scanWifiEvery(final int seconds) {
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                turnOnRequireServices();
+                handler.postDelayed(this, seconds * 1000);
+            }
+        };
+        //Start
+        handler.postDelayed(runnable, 0);
+    }
+
+    private void loadServices() {
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    private void turnOnRequireServices() {
+        turnOnGPS();
+        turnOnWifi();
+    }
+
+    private void turnOnWifi() {
+        // Turn on wifi if it is not enabled
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+
+        // Register receiver
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        // Start scan
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_CODE_ACCESS_LOCATION);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+
+        }else{
+            wifiManager.startScan();
+        }
+    }
+
+    private void turnOnGPS() {
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Intent turnOnGPSIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(turnOnGPSIntent);
+        }
     }
 
     private void loadView() {
@@ -44,9 +137,10 @@ public class WifiListActivity extends AppCompatActivity {
         btnDetail = findViewById(R.id.btnDetail);
     }
 
-    private void loadData(){
+    private void loadData() {
         // Get wifi list
-        wifiList = GetWifiDataFromJson.getWifiDataFromJSON(this);
+//        wifiList = GetWifiDataFromJson.getWifiDataFromJSON(this);
+        wifiList = new ArrayList<>();
         adapter = new WifiAdapter(wifiList, getApplicationContext());
         // If current selected item is changed, toggle button
         adapter.setItemClickListener(new ItemClickListener() {
@@ -72,5 +166,24 @@ public class WifiListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_LOCATION) {
+            wifiManager.startScan();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        unregisterReceiver(wifiReceiver);
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
     }
 }
